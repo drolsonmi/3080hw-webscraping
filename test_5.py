63c6f9c3f297ed1877c9dceb71b2dc7b9d7d484e
drolsonmi <dr.olsonmi@gmail.com>
2026-01-30 14:37:40 -0700

    Fixed test_5.py

--- test_5.py
+++ test_5.py
@@ -3,8 +3,11 @@ import pytest
 import pandas as pd
 import hw_webscraping as hw
 
+def test_docstrings():
+    assert hw.extract_nth_table.__doc__ != None, "The extract_nth_table function is missing a docstring."
+    assert hw.extract_all_links.__doc__ != None, "The extract_all_links function is missing a docstring."
+    assert hw.extract_table_rows.__doc__ != None, "The extract_table_rows function is missing a docstring."
+    assert hw.extract_nth_bold_text.__doc__ != None, "The extract_nth_bold_text function is missing a docstring."
 
-assert hw.extract_nth_table.__doc__ != None, "The extract_nth_table function is missing a docstring."
-assert hw.extract_all_links.__doc__ != None, "The extract_all_links function is missing a docstring."
-assert hw.extract_table_rows.__doc__ != None, "The extract_table_rows function is missing a docstring."
-assert hw.extract_nth_bold_text.__doc__ != None, "The extract_nth_bold_text function is missing a docstring."
+if __name__ == "__main__":
+    test_docstrings()
